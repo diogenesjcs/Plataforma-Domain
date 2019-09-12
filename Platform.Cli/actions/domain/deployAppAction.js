7159109202a6245c2fac50b272afdca77b0ad7c9
Diógenes <dsilva.pitang@ons.org.br>
2019-09-12 13:55:30 -0300

    deploy domain nova plataforma

--- Platform.Cli/actions/domain/deployAppAction.js
+++ Platform.Cli/actions/domain/deployAppAction.js
@@ -22,95 +22,38 @@ module.exports = class DeployAppAction extends BaseDeployAction {
         this.compiler = new CompileAppAction(appInstance);
     }
     deploy(_env) {
-        this.compiler.exec(_env)
-        .then(()=>{
-            return this.saveSystem(_env)
-        }).then(env =>{
-            this.createDockerContainer(env).then(() => {
-                if (env.conf.app.name !== "apicore") {
-                    this.saveDomainToApiCore(env).then(() => this.saveToApiCore(env)).catch(e => {
-                        console.log(e)
-                    });
-                    //this.saveToApiCore(env);
-                } else {
-                    console.log("App deployed");
-                }
-            });
-        });
-    }
-    createDockerContainer(env) {
-        return this.docker.compileDockerFile(env).then(() => {
-            return this.docker.build(env, env.docker.tag);
-        })/*.then(() => {
-        //    return this.docker.publish(env, env.docker.tag);
-        })*/.then(() => {
-            return this.docker.rm(env);
-        }).then(() => {
-            env.variables = {"DOMAIN_API":"1"};
-            return this.docker.run(env, env.docker.tag);
-        }).then(()=>{
-            return this.saveOperationCore(env,{
-                "event_in": env.conf.solution.id+".persist.request",
-                "event_out": env.conf.solution.id+".persist.done",
-                "image": env.docker.tag,
-                "version":env.conf.app.newVersion,
-                "name": env.conf.app.name+".persist",
-                "commit":true,
-                "reprocessable":true,
-                "processId": env.conf.app.id,
-                "systemId": env.conf.solution.id
-            });
-
-        }).then(()=>{
-            return this.saveOperationCore(env,{
-                "event_in": env.conf.solution.id+".merge.request",
-                "event_out": env.conf.solution.id+".merge.done",
-                "image": env.docker.tag,
-                "version":env.conf.app.newVersion,
-                "name": env.conf.app.name+".merge",
-                "commit":true,
-                "reprocessable":false,
-                "processId": env.conf.app.id,
-                "systemId": env.conf.solution.id
-            });
-
-        }).catch(e => {
-            console.log(e)
-        });
+        this.getDomainSchema().then((actualPath)=>this.importDomainMetadata(actualPath));
     }
 
-    saveSystem(env) {
+    getDomainSchema() {
         return new Promise((resolve,reject)=>{
-            var systemCore  = new System(env.apiCore)
-            systemCore.findById(env.conf.solution.id).then(found =>{
-                if (found.length === 0) {
-                    systemCore.create(env.conf.solution).then(()=>{
-                        resolve(env)
-                    }).catch(reject)
-                }else{
-                    resolve(env)
-                }
-            }).catch(reject)
+            var actualPath = shell.pwd();
+            var path = os.homedir()+"/installed_plataforma/domain_schema";
+            if (fs.existsSync(path)){
+                shell.cd(path);
+                shell.exec("git pull");
+            }else{
+                shell.rm("-rf",path);
+                shell.mkdir(path);
+                shell.cd(path);
+                shell.cd(" ..");
+                shell.exec("git clone https://github.com/onsplatform/domain_schema.git");
+            }
+            resolve(actualPath);
         })
     }
 
-    saveDomainToApiCore(env){
-        return new Promise((resolve, reject)=>{
-            this.domainModel = new DomainModelCore(env.apiCore);
-            var basePath = env.appPath+"/Dominio";
-            var entities = shell.ls(basePath).map(c => c);
-            var entities = entities.map(e => ({
-                systemId: env.conf.solution.id,
-                name:e.split(".")[0],
-                model:fs.readFileSync(basePath + "/"+e).toString(),
-                version:env.conf.app.newVersion
-            }))
-            this.domainModel.findBySystemId(env.conf.solution.id).then(regs => {
-                this.domainModel.destroy(regs).then(()=>{
-                    this.domainModel.create(entities).then(resolve).catch(reject);
-                }).catch(reject);
-            })
-        });
+    importDomainMetadata(actualPath) {
+        return new Promise((resolve,reject)=>{
+            var yamlPath = actualPath+"/Dominio";
+            var path = os.homedir()+"/installed_plataforma/domain_schema";
+            shell.cd(path);
+            shell.exec("pip install pipenv");
+            shell.exec("pipenv install");
+            shell.exec("set POSTGRES_HOST=localhost");
+            shell.exec("pipenv install pyyaml");
+            shell.exec("python manage.py import_data "+yamlPath+ " sager");
+        })
     }
 
 
